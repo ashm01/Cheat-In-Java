13878ccf9c5b30e187aec2428571b76fc41ac00f
ashm01 <a.moore2@uea.ac.uk>
2013-12-16 15:00:44 +0000

    Almost finished Card Class, just made difference as inner class - tested and works fine

--- src/question1/Card.java
+++ src/question1/Card.java
@@ -3,18 +3,7 @@ package question1;
 
 
 public class Card implements Comparable<Card> {
- 
-    
-    public enum Rank{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, 
-    NINE, TEN, JACK, QUEEN, KING;
-    
-        public Rank getNext(){
-            //Gets the next enum...... King.getNext returns Ace 
-            return values()[(ordinal()+1)%values().length];
-            }
-        }
-   
-    public enum Suit{CLUBS, DIAMONDS, HEARTS, SPACES}
+
     
     private Suit suit;
     private Rank rank;
@@ -24,6 +13,35 @@ public class Card implements Comparable<Card> {
         this.suit = suit;
     }
     
+    public enum Rank{ACE(1,11), TWO(2,2), THREE(3,3), FOUR(4,4), FIVE(5,5)
+    ,SIX(6,6), SEVEN(7,7), EIGHT(8,8), NINE(9,9), TEN(10,10),
+    JACK(11,10), QUEEN(12,10), KING(13,10);
+    
+        final int cardValue;
+        final int cardRankValue;
+        
+        Rank(int rank, int val){
+            
+            this.cardValue = val;
+            this.cardRankValue = rank;
+        }
+    
+        public Rank getNext(){
+            //Gets the next enum...... King.getNext returns Ace 
+            return values()[(ordinal()+1)%values().length];
+            }
+        
+        
+        public int getCardValue() {
+            return cardValue;
+        }
+
+        public int getRankValue() {
+            return cardRankValue;
+        }
+    }
+   
+    public enum Suit{CLUBS, DIAMONDS, HEARTS, SPADES}
     
      @Override
     public int compareTo(Card c) {
@@ -43,8 +61,36 @@ public class Card implements Comparable<Card> {
         return rank;
     }
      
-    public static int difference(Card c1, Card c2){
-        
-        
+    
+    
+    @Override
+    public String toString() {
+        return String.format("Card is %s of %s", this.rank, this.suit);
+    }
+    
+   
+   public static class Difference{
+       
+         
+     public static int difference(Card c, Card d){
+        int answer = 
+              Math.abs(d.getRank().getRankValue()- c.getRank().getRankValue());
+        return answer;
+    }
+    
+    public static int differenceValue(Card c, Card d){
+        int answer = 
+              Math.abs(d.getRank().getCardValue()- c.getRank().getCardValue()); 
+        return answer;
     } 
+       
+       
+      
+   } 
+   
+    
+    
+    
+    
+    
 }
--- src/question1/CardGameRuns.java
+++ src/question1/CardGameRuns.java
@@ -1,20 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package question1;
 
-/**
- *
- * @author Ash
- */
+import question1.Card.Rank;
+import question1.Card.Suit;
+
+
 public class CardGameRuns {
     
     public static void main(String[] args){
         
+    Card card1 = new Card(Rank.TEN, Suit.DIAMONDS);    
+    Card card2 = new Card(Rank.TEN, Suit.SPADES); 
+    Card card3 = new Card(Rank.TWO, Suit.CLUBS);    
+    Card card4 = new Card(Rank.SIX, Suit.HEARTS);
+    Card card5 = new Card(Rank.QUEEN, Suit.HEARTS);
+    
+        System.out.println(card3.compareTo(card4));
         
-        
-        
+        System.out.println(card1.toString());
+        //System.out.println(card1.difference(card2));
+        //System.out.println(card1.differenceValue(card5));
+        //System.out.println(card3.differenceValue(card4));
+        System.out.println(Card.Difference.difference(card1, card2));
+        System.out.println(Card.Difference.differenceValue(card1, card5));
+        System.out.println(Card.Difference.differenceValue(card3, card4));
     }
     
 }
