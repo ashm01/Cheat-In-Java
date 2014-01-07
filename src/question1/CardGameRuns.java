4320947148fbc5a14762f1353a30ae2195261cd8
Ash <Ash@Ash-PC.Home>
2014-01-07 16:08:19 +0000

    Finished hand class, everything is working fine all tested.

--- src/question1/CardGameRuns.java
+++ src/question1/CardGameRuns.java
@@ -24,24 +24,65 @@ public class CardGameRuns {
     Card card2 = new Card(Rank.TEN, Suit.SPADES); 
     Card card3 = new Card(Rank.TWO, Suit.CLUBS);    
     Card card4 = new Card(Rank.SIX, Suit.HEARTS);
-    Card card5 = new Card(Rank.QUEEN, Suit.HEARTS);
-    
-        System.out.println(card3.compareTo(card4));
-        
-        System.out.println(card1.toString());
+    Card card9 = new Card(Rank.QUEEN, Suit.HEARTS);
+    Card card6 = new Card(Rank.TEN, Suit.CLUBS);
+    Card card7 = new Card(Rank.ACE, Suit.HEARTS);
+    Card card8 = new Card(Rank.KING, Suit.HEARTS);
+    Card card5 = new Card(Rank.TEN, Suit.HEARTS);
+//        System.out.println(card3.compareTo(card4));
+//        
+//        System.out.println(card1.toString());
         //System.out.println(card1.difference(card2));
         //System.out.println(card1.differenceValue(card5));
         //System.out.println(card3.differenceValue(card4));
-        System.out.println(Card.Difference.difference(card1, card2));
-        System.out.println(Card.Difference.differenceValue(card1, card5));
-        System.out.println(Card.Difference.differenceValue(card3, card4));
+//        System.out.println(Card.Difference.difference(card1, card2));
+//        System.out.println(Card.Difference.differenceValue(card1, card5));
+//        System.out.println(Card.Difference.differenceValue(card3, card4));
         
-        List<Card> listOfCards = new ArrayList<Card>();
-        listOfCards.add(card1);
-        listOfCards.add(card2);
-        listOfCards.add(card3);
-        listOfCards.add(card4);
-        listOfCards.add(card5);
+        ArrayList<Card> col1 = new ArrayList<Card>();
+        col1.add(card1);
+        col1.add(card2);
+        col1.add(card3);
+        col1.add(card4);
+        col1.add(card5);
+        Hand hand = new Hand(col1);
+        
+        System.out.println(hand.countSuit(0));
+        System.out.println(hand.countRank(10));
+        System.out.println(hand.toString());
+        System.out.println();
+        hand.add(card6);
+        System.out.println(hand.countSuit(0));
+        System.out.println(hand.countRank(10));
+//        hand.remove(card6);
+        System.out.println(hand.countSuit(0));
+        System.out.println(hand.countRank(10));
+        
+        ArrayList<Card> col2 = new ArrayList<Card>();
+        col2.add(card7);
+        col2.add(card8);
+        col2.add(card9);
+        System.out.println("10's =" +hand.countRank(10));
+        System.out.println("Hearts ="+ hand.countSuit(2));
+        hand.add(col2);
+        System.out.println("10's =" +hand.countRank(10));
+        System.out.println("Hearts ="+ hand.countSuit(2));
+//        hand.remove(8);
+//        System.out.println("10's =" +hand.countRank(10));
+//        System.out.println("Hearts ="+ hand.countSuit(2));
+        Hand hand2 = new Hand(col2);
+        hand.remove(hand2);
+        System.out.println("10's =" +hand.countRank(10));
+        System.out.println("Hearts ="+ hand.countSuit(2));
+        hand.add(hand2);
+        System.out.println("10's =" +hand.countRank(10));
+        System.out.println("Hearts ="+ hand.countSuit(2));
+        hand.sortDescending();
+        System.out.println(hand.toString());
+        hand.sortAscending();
+        System.out.println(hand.toString());
+        System.out.println(hand2.isStraight());
+        System.out.println(hand2.isFlush());
         
 //        Collections.sort(listOfCards, new CompareDescending());
 //        for (Card c : listOfCards) {
@@ -52,18 +93,18 @@ public class CardGameRuns {
 //        for (Card c : listOfCards) {
 //		System.out.println(c);
 //	}
-        Deck deck1 = new Deck();
-//        deck1.display();
-//        deck1.shuffleDeck();
-        System.out.println("");
-//        deck1.display();
-        int count = 0;
-        Iterator<Card> it = deck1.iterator();
-        System.out.println(it.hasNext());
-        while(it.hasNext()){
-            System.out.println(count +" "+it.next());
-            count++;
-        }
+//        Deck deck1 = new Deck();
+////        deck1.display();
+////        deck1.shuffleDeck();
+//        System.out.println("");
+////        deck1.display();
+//        int count = 0;
+//        Iterator<Card> it = deck1.iterator();
+//        System.out.println(it.hasNext());
+//        while(it.hasNext()){
+//            System.out.println(count +" "+it.next());
+//            count++;
+//        }
         
         
 //        
--- src/question1/Hand.java
+++ src/question1/Hand.java
@@ -1,7 +1,194 @@
-
 package question1;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import question1.Card.CompareDescending;
 
-public class Hand {
+public class Hand implements Iterable<Card>, Serializable {
     
+    private final ArrayList<Card> hand;
+    private final double serialisationID = 102;
+    private int[] rankCount = new int[13];
+    private int[] suitCount = new int[4];
+    
+    public Hand() {
+        
+        this.hand = null;
+    }
+    
+    public Hand(ArrayList<Card> h) {
+        
+        this.hand = h;
+        suitAndRankCounter();
+    }
+    
+    public Hand(Hand h){
+        
+        hand = new ArrayList();
+        for (Card c : h){
+            
+            hand.add(c);
+            suitAndRankCounter();
+            
+        }
+    }
+    
+    
+    
+    private void suitAndRankCounter(){
+        for (Card c : hand) {
+        int rank = c.getRank().cardRankValue - 2;
+        int suit = c.getSuit().ordinal();    
+        rankCount[rank]++;
+        suitCount[suit]++;  
+        
+        }
+        
+    }
+    
+    public void add(Card c) {
+        int rank = c.getRank().cardRankValue - 2;
+        int suit = c.getSuit().ordinal();
+        hand.add(c);
+        rankCount[rank]++;
+        suitCount[suit]++;
+    }
+    
+    public void add(Collection<Card> cards) {
+        for (Card c : cards) {
+            int pos = c.getRank().cardRankValue - 2;
+            int suit = c.getSuit().ordinal();
+            hand.add(c);
+            rankCount[pos]++;
+            suitCount[suit]++;
+        }
+        
+    }
+    
+    public void add(Hand h) {
+        for (Card c : h) {
+            int pos = c.getRank().cardRankValue - 2;
+            int suit = c.getSuit().ordinal();
+            hand.add(c);
+            rankCount[pos]++; 
+            suitCount[suit]++;
+        }
+    }
+    
+    public boolean remove(Card c) {
+        for (Card temp : hand) {
+            if (temp == c) {
+                int pos = c.getRank().cardRankValue - 2;
+                int suit = c.getSuit().ordinal();
+                rankCount[pos]--; 
+                suitCount[suit]--;
+                hand.remove(c);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean remove(Hand h) {
+        int count = 0;
+        
+        Card[] temp = new Card[size(h)];
+        for (Card c : h) {
+            if (hand.contains(c)) {
+                int pos = c.getRank().cardRankValue - 2;
+                int suit = c.getSuit().ordinal();
+                rankCount[pos]--; 
+                suitCount[suit]--;
+                temp[count] = c;                
+                count++;
+            }else return false;
+            
+        }
+        for (int i = 0; i < temp.length; i++) {
+            hand.remove(temp[i]);
+        }
+        return true;
+    }
+    
+    public Card remove(int pos) {
+        Card temp = hand.get(pos);
+        int pos2 = temp.getRank().cardRankValue - 2;
+        int suit = temp.getSuit().ordinal();
+        rankCount[pos]--; 
+        suitCount[suit]--;
+        hand.remove(pos);
+        return temp;
+        
+    }
+    
+    @Override
+    public Iterator<Card> iterator() {
+        return hand.iterator();
+    }
+    
+    public int size(Hand h) {
+        
+        int size = 0;
+        for (Card c: h) {
+            size++;
+        }
+        
+        return size;
+    }
+    
+    public void sortAscending(){
+        
+        Collections.sort(hand);
+    }
+    
+    public void sortDescending(){
+        Collections.sort(hand, new CompareDescending());
+    }
+    
+    public int countRank(int rank){
+        
+        return rankCount[rank-2];
+    }
+    
+    public int countSuit(int suit){
+        
+        return suitCount[suit];
+    }
+    public int handValue(){
+        int handValue = 0;
+        for (Card c : hand) {
+            handValue = handValue + c.getRank().getCardValue();   
+        }
+        return handValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Hand{" + "hand=" + hand + '}';
+    }
+    
+    public boolean isFlush(){
+        
+        for (int i = 0; i < size(this)-1; i++) {
+            if (hand.get(i).getSuit().ordinal()==
+                    hand.get(i+1).getSuit().ordinal()) {
+                
+            }else return false;
+        }
+        return true;
+    }
+    
+    public boolean isStraight(){
+        this.sortAscending();
+        for (int i = 0; i < size(this)-1; i++) {
+            if (hand.get(i).getRank().getRankValue()+1==
+                    hand.get(i+1).getRank().getRankValue()) {
+                
+            }else return false;
+        }
+        return true;
+    }
 }
